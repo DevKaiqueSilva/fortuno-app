51979cff506955c4cb2792fd3dcf66a0d041dcc8
Kaique Silva <>
2025-10-23 09:27:53 -0300

    Fix cors for railway

--- fortuno-back/fortuno/settings.py
+++ fortuno-back/fortuno/settings.py
@@ -166,13 +166,38 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:9000",
     "http://127.0.0.1:9000",
     "https://fortuno-app.vercel.app",
-    "https://fortuno-app.vercel.app/",
 ]
 
 if os.environ.get('FRONTEND_URL'):
     CORS_ALLOWED_ORIGINS.append(os.environ.get('FRONTEND_URL'))
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
+]
+
+# Additional CORS headers
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 # Custom User Model
 AUTH_USER_MODEL = "app.User"
