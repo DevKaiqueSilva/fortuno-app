38f1884d58e0788181d18afb379b1cc8c8db6cfd
Kaique Silva <>
2025-10-23 09:20:15 -0300

    Add CORS

--- fortuno-back/fortuno/settings.py
+++ fortuno-back/fortuno/settings.py
@@ -165,6 +165,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:9000",
     "http://127.0.0.1:9000",
+    "https://fortuno-app.vercel.app",
 ]
 
 if os.environ.get('FRONTEND_URL'):
