917a9912743735786b597ee9e2067391568f228e
Kaique Silva <>
2025-10-24 09:28:51 -0300

    Fix

--- fortuno-back/fortuno/settings.py
+++ fortuno-back/fortuno/settings.py
@@ -29,9 +29,13 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = []
+
+# Railway.app hostname
 if os.environ.get('RAILWAY_ENVIRONMENT'):
-    ALLOWED_HOSTS.append('.railway.app')
+    ALLOWED_HOSTS = ['.railway.app']
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
