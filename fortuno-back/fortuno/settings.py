263116a07da2af6f9c26a14cc85322f75545a6ff
Kaique Silva <>
2025-10-23 21:00:25 -0300

    Fix

--- fortuno-back/fortuno/settings.py
+++ fortuno-back/fortuno/settings.py
@@ -166,7 +166,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "https://fortuno-app.vercel.app",
     "http://localhost:9000",
-    "http://localhost"
+    "http://localhost",
+    os.environ.get("FRONTEND_URL", "https://fortuno-app.vercel.app"),
 ]
 CORS_ALLOW_CREDENTIALS = True
 
