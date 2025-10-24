859f8ba610f52c73cf78f3e56df5129471ef825c
Kaique Silva <>
2025-10-23 21:05:54 -0300

    Fix

--- fortuno-back/fortuno/settings.py
+++ fortuno-back/fortuno/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -59,8 +60,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "app.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "fortuno.urls"
